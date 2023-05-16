6ffe137b5df505b2a55933adf8281e86ac47fca4
charitylefosa <charitylefosa@gmail.com>
2023-05-16 20:59:29 +0000

    Save 3-simple-shell final

--- 3-Simple_shell.c
+++ 3-Simple_shell.c
@@ -3,13 +3,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include"main.h"
 
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGS 64
 
-extern char **environ;
 
-void print_env()
+void read_prompt(char *command)
+{
+	printf("$ ");
+	fflush(stdout);
+	fgets(command, MAX_COMMAND_LENGTH, stdin);
+}
+
+void print_env(void)
 {
 	char **env = environ;
 
@@ -19,60 +26,68 @@ void print_env()
 		env++;
 	}
 }
+int write_command(char *command, char **args)
+{
+	int i = 0;
+	char *token;
+
+	token = strtok(command, " ");
+	while (token != NULL && i < MAX_ARGS - 1)
+	{
+		args[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
+	return (i);
+}
+
+void execute_command(char **args)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		fprintf(stderr, "Error: Fork failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execvp(args[0], args);
+		fprintf(stderr, "./shell: No such file or directory\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
+
 
 int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
 	char *args[MAX_ARGS];
 	int running = 1;
-	pid_t pid;
-	int i;
-	char *token;
+	int argc;
 
 	while (running)
 	{
-		printf("$ ");
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-		{
-			printf("\n");
-			break;
-		}
+		read_prompt(command);
 		command[strcspn(command, "\n")] = '\0';
-		i = 0;
-		token = strtok(command, " ");
-		while (token != NULL && i < MAX_ARGS -1)
+		argc = write_command(command, args);
+		if (argc > 0)
 		{
-			args[i] = token;
-			token = strtok(NULL, " ");
-			i++;
-		}
-		args[i] = NULL;
-		if (i == 0)
-			continue;
-		if (strcmp(args[0], "env") == 0)
-		{
-			print_env();
-			continue;
-		}
-		if (strcmp(args[0], "exit") == 0)
-		{
-			exit(EXIT_SUCCESS);
-		}
-		pid = fork();
-		if (pid < 0)
-		{
-			fprintf(stderr, "Error: Fork failed.\n");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			execvp(args[0], args);
-			fprintf(stderr, "./shell: No such file or directory\n");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			wait(NULL);
+			if (strcmp(args[0], "env") == 0)
+			{
+				print_env();
+				continue;
+			}
+			if (strcmp(args[0], "exit") == 0)
+				exit(EXIT_SUCCESS);
+			else
+				execute_command(args);
 		}
 	}
 	return (0);
